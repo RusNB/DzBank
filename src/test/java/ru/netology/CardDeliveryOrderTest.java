f8632a562ee774183b8d3f2a3e8a5bf0dea54834
RusNB <balushkin-007@mail.ru>
2025-01-11 15:18:33 +0300

    New Test

--- src/test/java/ru/netology/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -13,21 +14,23 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest {
-    String date = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
     @BeforeEach
     void setUp() {
         Selenide.open("http://localhost:9999/");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
     }
-
     @Test
-    void shouldTestV6() {
+    void shouldTestV1() {
         $("[data-test-id='city'] input").setValue("Краснодар");
         $("[data-test-id='date'] input").setValue(date);
         $("[data-test-id='name'] input").setValue("Ефимов Александр");
         $("[data-test-id='phone'] input").setValue("+79256678765");
+        $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(".input_invalid .checkbox__text")
-                .shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + date), Duration.ofSeconds(15));
     }
+
+
 }
